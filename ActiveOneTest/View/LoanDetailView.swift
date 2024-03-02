551c2d84dbf701dae84373e85e4a08940b4ea998
Xavier Sotomayor <xaviersotomayor1989@gmail.com>
2024-03-02 14:46:29 -0500

    Update LoanDetailView.swift

--- ActiveOneTest/View/LoanDetailView.swift
+++ ActiveOneTest/View/LoanDetailView.swift
@@ -5,8 +5,6 @@
 //  Created by Xavier Sotomayor on 2/3/24.
 //
 
-import Foundation
-
 import SwiftUI
 
 struct LoanDetailView: View {
@@ -15,19 +13,19 @@ struct LoanDetailView: View {
     
     var body: some View {
         VStack(alignment: .leading, spacing: 30) {
-            Button(action: {
-          
-            }, label: {
-                HStack(spacing: 4) {
-                    Image(systemName: "arrow.left")
-                        .resizable()
-                        .frame(width: 20, height: 14, alignment: .center)
-                }
-            })
-            .frame(width: 50, height: 50, alignment: .center)
-            .accentColor(.black)
-            .background(Color.gray.opacity(0.2))
-            .cornerRadius(25)
+//            Button(action: {
+//          
+//            }, label: {
+//                HStack(spacing: 4) {
+//                    Image(systemName: "arrow.left")
+//                        .resizable()
+//                        .frame(width: 20, height: 14, alignment: .center)
+//                }
+//            })
+//            .frame(width: 50, height: 50, alignment: .center)
+//            .accentColor(.black)
+//            .background(Color.gray.opacity(0.2))
+//            .cornerRadius(25)
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 550, alignment: .topLeading)
         .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
@@ -42,9 +40,3 @@ struct LoanDetailView_Previews: PreviewProvider {
         LoanDetailView()
     }
 }
-
-
-
-
-
-
