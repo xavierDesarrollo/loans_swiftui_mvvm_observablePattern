b2261d640cde1e3eb4359b2955240965bddd0527
Xavier Sotomayor <xaviersotomayor1989@gmail.com>
2024-03-04 08:33:46 -0500

    Update LoanDetailView.swift

--- ActiveOneTest/View/LoanDetailView.swift
+++ ActiveOneTest/View/LoanDetailView.swift
@@ -9,25 +9,31 @@ import SwiftUI
 
 struct LoanDetailView: View {
     
-    //@StateObject var viewModel = LoanDetailViewModel()
+    var loan: Loan!
     
     var body: some View {
         VStack(alignment: .leading, spacing: 30) {
-//            Button(action: {
-//          
-//            }, label: {
-//                HStack(spacing: 4) {
-//                    Image(systemName: "arrow.left")
-//                        .resizable()
-//                        .frame(width: 20, height: 14, alignment: .center)
-//                }
-//            })
-//            .frame(width: 50, height: 50, alignment: .center)
-//            .accentColor(.black)
-//            .background(Color.gray.opacity(0.2))
-//            .cornerRadius(25)
+            VStack(alignment: .leading) {
+                Text("Due in 0 days").font(.system(size: 14).bold()).foregroundStyle(.gray)
+                Text("$0").font(.system(size: 30, design: .rounded).bold())
+            }
+            HStack(alignment: .center, spacing: 3) {
+                Text("Next payment").font(.system(size: 14).bold()).foregroundStyle(.gray)
+                Text("0").font(.system(size: 15).bold())
+                Spacer()
+                Text("0").font(.system(size: 15).bold())
+                Text("of 0 payments").font(.system(size: 14).bold()).foregroundStyle(.gray)
+            }
+            Button(action: {
+                        
+            }) {
+                Text("Make a payment").font(.system(size: 14).bold()).foregroundStyle(.white)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50, alignment: .center)
+            .background(Color.loanBackgroundPaymentButton)
+            .cornerRadius(25)
         }
-        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 550, alignment: .topLeading)
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 550, alignment: .center)
         .padding(EdgeInsets(top: 20, leading: 20, bottom: 0, trailing: 20))
         .background(Color.loanBackground)
         .cornerRadius(30)
@@ -40,3 +46,9 @@ struct LoanDetailView_Previews: PreviewProvider {
         LoanDetailView()
     }
 }
+
+
+
+
+
+
